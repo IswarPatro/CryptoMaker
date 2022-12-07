0aa8c40e18a58433f55e3bff87053f0fe4c716a2
Iswar Patro <iswarpatro11@gmail.com>
2022-12-07 11:36:12 +0530

    first commit

--- src/components/Authentication/UserSidebar.js
+++ src/components/Authentication/UserSidebar.js
@@ -72,123 +72,128 @@ const useStyles = makeStyles({
 export default function UserSidebar() {
   const classes = useStyles();
   const [state, setState] = React.useState({
-       right: false,
+    right: false,
   });
-  const {user,setAlert,watchlist,coins,symbol}=CryptoState();
+  const { user, setAlert, watchlist, coins, symbol } = CryptoState();
 
-  
+  console.log(watchlist, coins);
+
   const toggleDrawer = (anchor, open) => (event) => {
-    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
       return;
     }
 
     setState({ ...state, [anchor]: open });
   };
-  const removeFromWatchlist=async(coin)=>{
-    const coinRef=doc(db,"watchlist",user.uid);
 
+  const logOut = () => {
+    signOut(auth);
+    setAlert({
+      open: true,
+      type: "success",
+      message: "Logout Successfull !",
+    });
+
+    toggleDrawer();
+  };
+
+  const removeFromWatchlist = async (coin) => {
+    const coinRef = doc(db, "watchlist", user.uid);
     try {
       await setDoc(
         coinRef,
-        {coins:watchlist.filter((watch)=>watch!==coin?.id)},
-        {merge:"true"}
-        );
-        setAlert({
-          open:true,
-          message:`${coin.name} Removed from the Watchlist !`,
-          type:"success",
-        });
+        { coins: watchlist.filter((wish) => wish !== coin?.id) },
+        { merge: true }
+      );
+
+      setAlert({
+        open: true,
+        message: `${coin.name} Removed from the Watchlist !`,
+        type: "success",
+      });
     } catch (error) {
       setAlert({
-        open:true,
-        message:error.message,
-        type:"error",
+        open: true,
+        message: error.message,
+        type: "error",
       });
     }
-  }
-
-  const logOut=()=>{
-    signOut(auth); 
-    setAlert({
-        open:true,
-        type:"success",
-        message:"Logout Successfully !",
-    });
-    toggleDrawer();
-  }; 
+  };
 
   return (
     <div>
-      {['right'].map((anchor) => (
+      {["right"].map((anchor) => (
         <React.Fragment key={anchor}>
-          <Avatar onClick={toggleDrawer(anchor,true)}
+          <Avatar
+            onClick={toggleDrawer(anchor, true)}
             style={{
-                height:38,
-                width:38,
-                cursor:"pointer",
-                backgroundColor:"#97CFD5",
+              height: 38,
+              width: 38,
+              marginLeft: 15,
+              cursor: "pointer",
+              backgroundColor: "#EEBC1D",
             }}
             src={user.photoURL}
             alt={user.displayName || user.email}
           />
-          <Drawer anchor={anchor} open={state[anchor]} onClose={toggleDrawer(anchor, false)}>
-           <div className={classes.container}>
-                <div className={classes.profile}>
-                    <Avatar 
-                        className={classes.picture}
-                        src={user.photoURL}
-                        alt={user.displayName || user.email}
-                    />
-                    <span
-                        style={{
-                            width:"100%",
-                            fontSize:25,
-                            textAlign:"center",
-                            fontWeight:"bolder",
-                            wordWrap:"break-word",
-
-                        }}
-                    >
-                        {user.displayName || user.email}
-                    </span>
-                    <div className={classes.watchlist}>
-                        <span
-                            style={{ fontSize:15,textShadow: "0 0 5px black" }}
-                        >
-                            Watchlist
-                        </span>
-                          {
-                            // eslint-disable-next-line
-                            coins.map((coin)=>{
-                              if(watchlist.includes(coin.id))
-                              return (
-                                <div className={classes.coin}>
-                                  <span>{coin.name}</span>
-                                  <span style={{ display :" flex ",gap: 8 }}>
-                                    {symbol}
-                                    {numberWithCommas(coin.current_price.toFixed(2))}
-                                    <AiFillDelete 
-                                      style={{
-                                        cursor:"pointer"
-                                      }}
-                                      fontSize="16"
-                                      onClick={()=>removeFromWatchlist(coin)}
-                                    />
-                                  </span>
-                                </div>
-                              )
-                            })
-                          }
-                    </div>
-                </div>
-                <Button
-                    variant="contained"
-                    className={classes.logout}
-                    onClick={logOut}
+          <Drawer
+            anchor={anchor}
+            open={state[anchor]}
+            onClose={toggleDrawer(anchor, false)}
+          >
+            <div className={classes.container}>
+              <div className={classes.profile}>
+                <Avatar
+                  className={classes.picture}
+                  src={user.photoURL}
+                  alt={user.displayName || user.email}
+                />
+                <span
+                  style={{
+                    width: "100%",
+                    fontSize: 25,
+                    textAlign: "center",
+                    fontWeight: "bolder",
+                    wordWrap: "break-word",
+                  }}
                 >
-                    Log Out
-                </Button>
-           </div>
+                  {user.displayName || user.email}
+                </span>
+                <div className={classes.watchlist}>
+                  <span style={{ fontSize: 15, textShadow: "0 0 5px black" }}>
+                    Watchlist
+                  </span>
+                  {coins.map((coin) => {
+                    if (watchlist.includes(coin.id))
+                      return (
+                        <div className={classes.coin}>
+                          <span>{coin.name}</span>
+                          <span style={{ display: "flex", gap: 8 }}>
+                            {symbol}{" "}
+                            {numberWithCommas(coin.current_price.toFixed(2))}
+                            <AiFillDelete
+                              style={{ cursor: "pointer" }}
+                              fontSize="16"
+                              onClick={() => removeFromWatchlist(coin)}
+                            />
+                          </span>
+                        </div>
+                      );
+                    else return <></>;
+                  })}
+                </div>
+              </div>
+              <Button
+                variant="contained"
+                className={classes.logout}
+                onClick={logOut}
+              >
+                Log Out
+              </Button>
+            </div>
           </Drawer>
         </React.Fragment>
       ))}
--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -1,3 +1,4 @@
+import React from 'react';
 function ErrorFallback({error, resetErrorBoundary}) {
   return (
     <div role="alert">
